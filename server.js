992ed9203c52f77d480e4a9783553d81db4f552c
kaligineedimadhavi@gmail.com <146701005+m12vi@users.noreply.github.com>
2024-08-29 23:18:41 +0530

    firstcommit

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.get('*',function(req,res){
     res.sendFile(path.join(__dirname,'./client/build/index.html'));
 });
 //port
-const PORT = process.env.PORT || 8080;
+const PORT =process.env.PORT || 8080;
 
 //listen server
 app.listen(PORT, () =>{
